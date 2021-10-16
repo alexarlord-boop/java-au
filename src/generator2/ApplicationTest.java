433822f5cd40367778955859c33ab11ab0eb475e
alexarlord-boop <rezerw0.1@yandex.ru>
2021-10-17 00:03:42 +0300

    GENERATOR2-2022 Added ApplicationTest.java realisation

--- src/generator2/ApplicationTest.java
+++ src/generator2/ApplicationTest.java
@@ -2,11 +2,45 @@ package generator2;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest {
 
+    public static final List<String> OLD_CONTENT = Collections.singletonList("");
+    public static final List<String> USER_CONTENT = Arrays.asList("Reverse Linked List",
+            "\n",
+            "https://leetcode.com/problems/reverse-linked-list/",
+            "\n",
+            "while head != None:",
+            "    prev = head",
+            "    head = n_node",
+            "return prev\n");
+
+    public static final String RESULT = """
+            # TESTOUTPUT
+
+            + [Reverse Linked List](#reverse-linked-list)
+            <!---->
+
+            ## Reverse Linked List
+
+            https://leetcode.com/problems/reverse-linked-list/
+
+            ```java
+            while head != None:
+                prev = head
+                head = n_node
+            return prev
+            ```
+            """;
+
     @Test
     void generateContent() {
+        assertEquals(RESULT, Application.generateContent(OLD_CONTENT, USER_CONTENT, Application.FileType.MARKDOWN, "testoutput"));
     }
 }
